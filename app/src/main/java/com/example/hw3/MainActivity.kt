533d775473f8d25957e8f7a8f1390478ca43025e
AskarBekB <askarbekbergen30@gmail.com>
2023-11-17 06:55:55 +0600

    Added the functions, preview functions

--- app/src/main/java/com/example/hw3/MainActivity.kt
+++ app/src/main/java/com/example/hw3/MainActivity.kt
@@ -3,13 +3,34 @@ package com.example.hw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.hw3.data.DataSource
+import com.example.hw3.model.Topic
 import com.example.hw3.ui.theme.HW3Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +43,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier){
+    Card {
+        Row {
+            Box{
+               Image(
+                   painter = painterResource(id = topic.imageResourceId),
+                   contentDescription = null,
+                   modifier = modifier
+                       .size(width = 70.dp, height = 70.dp)
+                       .aspectRatio(1f),
+                   contentScale = ContentScale.Crop
+               )
+            }
+            Column {
+                Text(
+                    text = stringResource(id = topic.stringResourceId),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(id = R.dimen.padding_medium),
+                        top = dimensionResource(id = R.dimen.padding_medium),
+                        end = dimensionResource(id = R.dimen.padding_medium),
+                        bottom = dimensionResource(id = R.dimen.padding_small)
+                    )
+                )
+                Row(verticalAlignment = Alignment.CenterVertically){
+                    Icon(
+                        painter = painterResource(id = R.drawable.icon),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = dimensionResource(id = R.dimen.padding_medium))
+                    )
+                    Text(
+                        text = topic.numCourse.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_small))
+                    )
                 }
             }
         }
@@ -30,17 +96,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HW3Theme {
-        Greeting("Android")
+fun TopicGrid(modifier: Modifier = Modifier){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small)),
+    ){
+        items(DataSource.topics){topic ->
+            TopicCard(topic = topic)
+        }
     }
 }
+
+@Preview()
+@Composable
+fun TopicPreview(){
+    HW3Theme {
+        val topic = Topic(R.string.photography, 321, R.drawable.photography)
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+            ){
+            TopicCard(topic = topic)
+        }
+    }
+}
+
